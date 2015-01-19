fe7c3c2e1180aeaca125f19148242fe2127db6a4
Diego-Andrade <bets636@gmail.com>
2015-01-19 15:37:27 -0800

    Edit Hay squeezer to work

--- Heimdall/src/Commands/haySqueezerToggle.cpp
+++ Heimdall/src/Commands/haySqueezerToggle.cpp
@@ -39,7 +39,7 @@ void haySqueezerToggle::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool haySqueezerToggle::IsFinished() {
-	return false;
+	return true;
 }
 
 // Called once after isFinished returns true
