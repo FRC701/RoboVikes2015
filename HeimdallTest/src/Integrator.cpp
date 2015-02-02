2ef7cdc5eab35e92b8919fd10aa48b06977611cd
Diego-Andrade <bets636@gmail.com>
2015-02-01 17:04:22 -0800

    Added Integrator for reals....

---
+++ HeimdallTest/src/Integrator.cpp
@@ -0,0 +1,33 @@
+/*
+ * Integrator.cpp
+ *
+ *  Created on: Feb 1, 2015
+ *      Author: vandenrobotics
+ */
+
+#include "Integrator.h"
+
+Integrator::Integrator()
+: sum(0.0),
+  previous(0.0)
+{
+}
+
+void Integrator::push(double val)
+{
+	sum += (previous + val) / 2;
+	previous = val;
+}
+
+void Integrator::reset()
+{
+	sum = 0.0;
+}
+
+double Integrator::get()
+{
+	return sum;
+}
+
+
+
