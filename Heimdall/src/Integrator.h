0d1f4180302ccdf7659c2183c05415e26a2e6844
Diego-Andrade <bets636@gmail.com>
2015-02-03 17:21:13 -0800

    Added Accelerometer Class and other dependent classes
    - MovingAverage, Integrator

---
+++ Heimdall/src/Integrator.h
@@ -0,0 +1,22 @@
+/*
+ * Integrator.h
+ *
+ *  Created on: Feb 1, 2015
+ *      Author: vandenrobotics
+ */
+
+#ifndef INTEGRATOR_H_
+#define INTEGRATOR_H_
+
+class Integrator {
+public:
+	Integrator();
+	void push(double val);
+	void reset();
+	double get() const;
+private:
+	double sum;
+	double previous;
+};
+
+#endif /* INTEGRATOR_H_ */
---
+++ Heimdall/src/MovingAverage.cpp
@@ -0,0 +1,54 @@
+/*
+ * MovingAverage.cpp
+ *
+ *  Created on: Jan 30, 2015
+ *      Author: vandenrobotics
+ */
+
+#include "MovingAverage.h"
+
+MovingAverage::MovingAverage(size_t size)
+: maxSize(size),
+  sum(0.0)
+{
+}
+
+double MovingAverage::returnAverage() const
+{
+	if (values.size() == 0)
+		return 0;
+
+	double average = sum / values.size();
+	return average;
+}
+
+void MovingAverage::push(double value)
+{
+		values.push_front(value);
+		sum += value;
+		if(values.size() > maxSize)
+		{
+			sum -= values.back();
+			values.pop_back();
+		}
+}
+
+double MovingAverage::returnSum()
+{
+	return sum;
+}
+
+double MovingAverage::returnIndex(int index)
+{
+	return values[index];
+}
+
+double MovingAverage::returnSize()
+{
+	return values.size();
+}
+
+void MovingAverage::reset()
+{
+	values.clear();
+}
