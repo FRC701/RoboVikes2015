3c915d367bc9b0305e0d3ecfd7775afeb6ab529f
DarkCyloid <kandle.kristofer01@gmail.com>
2015-02-12 15:47:01 -0800

    Added to AutoLeftForwardDrive

--- Heimdall/src/Commands/AutoRightForwardDrive.cpp
+++ Heimdall/src/Commands/AutoRightForwardDrive.cpp
@@ -57,6 +57,4 @@ void AutoRightForwardDrive::End() {
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void AutoRightForwardDrive::Interrupted() {
-	timer.Stop();
-	timer.Reset();
 }
