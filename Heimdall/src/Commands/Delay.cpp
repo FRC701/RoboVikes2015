1993921eb7930a07dfd078f399b03470462deed4
aaronistheman <aarons.7007@gmail.com>
2015-03-06 15:28:29 -0800

    Removed some SmartDashboard prints in the Delay command and changed the autonomous commands that open the hay squeezer so that they do not depend on the currently not working pneumatic sensor

--- Heimdall/src/Commands/Delay.cpp
+++ Heimdall/src/Commands/Delay.cpp
@@ -32,16 +32,10 @@ void Delay::End()
 {
 	mTimer.Stop();
 	mTimer.Reset();
-	
-	// For testing purposes:
-	SmartDashboard::PutNumber("Delay Duration", Robot::prefs->GetDouble("DelayTestDelay", 0.0));
 }
 
 void Delay::Interrupted()
 {
 	mTimer.Stop();
 	mTimer.Reset();
-	
-	// For testing purposes:
-	SmartDashboard::PutNumber("Delay Duration", Robot::prefs->GetDouble("DelayTestDelay", 0.0));
 }
