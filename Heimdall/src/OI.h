c5b87a347a34a87c9b3fd27c15037a7cce1296ea
aaronistheman <aarons.7007@gmail.com>
2015-03-08 10:24:49 -0700

    Implemented a setpoint for pulling out totes via the ice picks

--- Heimdall/src/OI.h
+++ Heimdall/src/OI.h
@@ -31,6 +31,7 @@ private:
 	JoystickButton* cWhite;
 	JoystickButton* cBlack;
 	JoystickButton* cRed;
+	JoystickButton* cGreenLeft;
 //*/
 //Guitar......................................................................
 /*
